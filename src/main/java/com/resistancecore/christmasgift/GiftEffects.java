37a66996d49f5885ac0e73a864472e5a1de4b167
ResistanceCore <resistancecoreplugin@gmail.com>
2025-12-03 19:45:28 +0700

    Optimize particle effects in GiftEffects.java
    Reduced the number of steps and particles in various effects for better performance and fixing spam in console.

--- src/main/java/com/resistancecore/christmasgift/GiftEffects.java
+++ src/main/java/com/resistancecore/christmasgift/GiftEffects.java
@@ -18,7 +18,7 @@ public class GiftEffects {
         
         new BukkitRunnable() {
             private int step = 0;
-            private final int STEPS = 30; // Reduced from 40
+            private final int STEPS = 30;
             private double angle = 0;
 
             @Override
@@ -28,17 +28,17 @@ public class GiftEffects {
                     return;
                 }
 
-                if (step < 8) { // Reduced from 10
+                if (step < 8) {
                     // Initial burst
                     playSound(player, Sound.BLOCK_NOTE_BLOCK_CHIME, 1.0F, 1.0F + (step * 0.1F));
                     spawnSpiral(loc, step);
-                } else if (step < 16) { // Reduced from 20
+                } else if (step < 16) {
                     // Rising particles
                     spawnRisingParticles(loc);
-                    if (step % 2 == 0) { // Play sound every other tick
+                    if (step % 2 == 0) {
                         playSound(player, Sound.BLOCK_NOTE_BLOCK_BELL, 1.0F, 1.0F + ((step-8) * 0.1F));
                     }
-                } else if (step < 24) { // Reduced from 30
+                } else if (step < 24) {
                     // Explosion effect
                     spawnFireworks(loc);
                     if (step == 16) {
@@ -49,61 +49,53 @@ public class GiftEffects {
                     spawnSparkles(loc);
                 }
 
-                angle += Math.PI / 6; // Slightly faster rotation
+                angle += Math.PI / 6;
                 step++;
             }
 
             private void spawnSpiral(Location loc, int step) {
-                // Reduced particles for better performance
-                for (double i = 0; i < Math.PI * 2; i += Math.PI / 4) { // Reduced from PI/8
+                for (double i = 0; i < Math.PI * 2; i += Math.PI / 4) {
                     double x = Math.cos(i + angle) * (step * 0.2);
                     double z = Math.sin(i + angle) * (step * 0.2);
-                    loc.add(x, 0.5, z);
-                    player.getWorld().spawnParticle(Particle.FLAME, loc, 1, 0, 0, 0, 0);
-                    loc.subtract(x, 0.5, z);
+                    Location spiralLoc = loc.clone().add(x, 0.5, z);
+                    player.getWorld().spawnParticle(Particle.FLAME, spiralLoc, 1);
                 }
             }
 
             private void spawnRisingParticles(Location loc) {
-                // Reduced particles for better performance
-                for (int i = 0; i < 6; i++) { // Reduced from 8
+                for (int i = 0; i < 6; i++) {
                     double x = Math.cos(angle + (i * Math.PI / 3)) * 0.8;
                     double z = Math.sin(angle + (i * Math.PI / 3)) * 0.8;
-                    loc.add(x, 0, z);
-                    player.getWorld().spawnParticle(Particle.SNOWFLAKE, loc, 1, 0, 0, 0, 0.1);
-                    player.getWorld().spawnParticle(Particle.END_ROD, loc, 1, 0, 0, 0, 0.05);
-                    loc.subtract(x, 0, z);
+                    Location risingLoc = loc.clone().add(x, 0, z);
+                    player.getWorld().spawnParticle(Particle.CLOUD, risingLoc, 1);
+                    player.getWorld().spawnParticle(Particle.CRIT, risingLoc, 1);
                 }
             }
 
             private void spawnFireworks(Location loc) {
-                // Reduced particles for better performance
-                for (int i = 0; i < 12; i++) { // Reduced from 16
+                for (int i = 0; i < 12; i++) {
                     double x = Math.cos(i * Math.PI / 6) * 1.5;
                     double z = Math.sin(i * Math.PI / 6) * 1.5;
-                    loc.add(x, 1, z);
-                    player.getWorld().spawnParticle(Particle.WITCH, loc, 1, 0, 0, 0, 0);
-                    player.getWorld().spawnParticle(Particle.FLAME, loc, 1, 0, 0, 0, 0);
-                    loc.subtract(x, 1, z);
+                    Location fireworkLoc = loc.clone().add(x, 1, z);
+                    player.getWorld().spawnParticle(Particle.FLAME, fireworkLoc, 1);
+                    player.getWorld().spawnParticle(Particle.CRIT, fireworkLoc, 1);
                 }
             }
 
             private void spawnSparkles(Location loc) {
-                // Reduced particles for better performance
-                for (int i = 0; i < 3; i++) { // Reduced from 4
-                    double x = (Math.random() - 0.5) * 2.5; // Reduced range
-                    double y = Math.random() * 1.5; // Reduced range
-                    double z = (Math.random() - 0.5) * 2.5; // Reduced range
-                    loc.add(x, y, z);
-                    player.getWorld().spawnParticle(Particle.INSTANT_EFFECT, loc, 1, 0, 0, 0, 0);
-                    player.getWorld().spawnParticle(Particle.FIREWORK, loc, 1, 0, 0, 0, 0);
-                    loc.subtract(x, y, z);
+                for (int i = 0; i < 3; i++) {
+                    double x = (Math.random() - 0.5) * 2.5;
+                    double y = Math.random() * 1.5;
+                    double z = (Math.random() - 0.5) * 2.5;
+                    Location sparkleLoc = loc.clone().add(x, y, z);
+                    player.getWorld().spawnParticle(Particle.CRIT_MAGIC, sparkleLoc, 2);
+                    player.getWorld().spawnParticle(Particle.ENCHANT, sparkleLoc, 2);
                 }
             }
 
             private void playSound(Player player, Sound sound, float volume, float pitch) {
                 player.playSound(player.getLocation(), sound, volume, pitch);
             }
-        }.runTaskTimer(plugin, 0L, 2L); // Changed to 2L (every 2 ticks) for better performance
+        }.runTaskTimer(plugin, 0L, 2L);
     }
 }
